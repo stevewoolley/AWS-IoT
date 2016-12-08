e74d5b6d05d634f6700c4b3f6979d5d2c6b928ad
Steve Woolley <steve.woolley@me.com>
2016-12-07 22:14:55 -0500

    make publisher use AWSIoTPythonSDK

--- publisher.py
+++ publisher.py
@@ -1,42 +1,47 @@
 import logging
 import util
-import paho.mqtt.publish as publish
-from paho.mqtt.client import MQTTv311
-import ssl
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 
 class Publisher:
-    """A MQTT Publisher object"""
+    """A threaded Publisher object"""
 
-    def __init__(self, endpoint, root_ca, key, cert, port=8883, log_level=logging.INFO):
+    def __init__(self, endpoint, root_ca, key, cert, client_id='', web_socket=False, log_level=logging.INFO):
         self.endpoint = endpoint
         self.root_ca = root_ca
+        self.client_id = client_id
+        self.client = None
+        self.web_socket = web_socket
         self.key = key
         self.cert = cert
-        self.port = port
         self.logger = util.set_logger(level=log_level)
+        self.connected = False
 
-    def publish(self, topic, obj, qos=0, retain=False):
-        msg = {'topic': topic, 'payload': obj, 'qos': qos, 'retain': retain}
-        publish.multiple([msg],
-                         hostname=self.endpoint,
-                         port=self.port,
-                         tls={'ca_certs': self.root_ca,
-                              'certfile': self.cert,
-                              'keyfile': self.key,
-                              'tls_version': ssl.PROTOCOL_TLSv1_2
-                              }
-                         ,
-                         protocol=MQTTv311)
+    def connect(self):
+        # Setup
+        if self.web_socket:
+            self.client = AWSIoTMQTTClient(self.client_id, useWebsocket=True)
+            self.client.configureEndpoint(self.endpoint, 443)
+            self.client.configureCredentials(self.root_ca)
+        else:
+            self.client = AWSIoTMQTTClient(self.client_id)
+            self.client.configureEndpoint(self.endpoint, 8883)
+            self.client.configureCredentials(self.root_ca, self.key, self.cert)
+        self.client.configureAutoReconnectBackoffTime(1, 32, 20)
+        self.client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        self.client.configureDrainingFrequency(2)  # Draining: 2 Hz
+        self.client.configureConnectDisconnectTimeout(10)  # 10 sec
+        self.client.configureMQTTOperationTimeout(5)  # 5 sec
+        # Connect
+        self.connected = self.client.connect()
 
-    def publish_multiple(self, obj):
-        publish.multiple(obj,
-                         hostname=self.endpoint,
-                         port=self.port,
-                         tls={'ca_certs': self.root_ca,
-                              'certfile': self.cert,
-                              'keyfile': self.key,
-                              'tls_version': ssl.PROTOCOL_TLSv1_2
-                              }
-                         ,
-                         protocol=MQTTv311)
+    def publish(self, topic, obj, qos=0):
+        if not self.connected:
+            self.connect()
+        self.client.publish(topic, obj, qos)
+
+    def publish_multiple(self, objects, qos=0):
+        if not self.connected:
+            self.connect()
+        for obj in objects:
+            self.client.publish(obj['topic'], obj['payload'], qos)
