e5bb92e088a63cff9571b66055a1acd701fd7b1a
Steve Woolley <steve.woolley@me.com>
2016-12-09 21:35:21 -0500

    added thing name to pi_monitor

--- pi_monitor.py
+++ pi_monitor.py
@@ -50,3 +50,4 @@ args = parser.parse_args()
 # Lookup system_info
 thing = Thing(args.name, args.endpoint, args.rootCA, args.key, args.cert)
 thing.update({'state': {'reported': get_properties(args.party)}})
+thing.finish = True
