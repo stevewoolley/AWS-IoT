377d76003869771154e7749ee77dc3bbac8e80d4
Steve Woolley <steve.woolley@me.com>
2016-12-21 22:36:33 -0500

    tranistion to  paho mqtt

--- camera_sub.py
+++ camera_sub.py
@@ -11,7 +11,7 @@ from cloud_tools import Publisher
 from camera import Camera
 
 STORAGE_DIRECTORY = '/tmp'
-SNAP_FILENAME = 'snapshot.png'
+IMAGE_FILE_EXT = 'png'
 DATE_FORMAT = '%Y_%m_%d_%H_%M_%S'
 MQTT_PORT = 8883
 MQTT_KEEPALIVE = 60
@@ -20,33 +20,33 @@ MQTT_KEEPALIVE = 60
 def on_message(mqttc, obj, msg):
     logger.info("on_message {} {} {}".format(msg.topic, msg.qos, msg.payload))
     # payload = json.loads(msg.payload)
-    if camera.snap('/'.join((STORAGE_DIRECTORY, SNAP_FILENAME))):
-        filename, file_extension = os.path.splitext(SNAP_FILENAME)
-        f = "{}_{}{}".format(datetime.datetime.now().strftime(DATE_FORMAT), args.source, file_extension)
-        logger.info("on_message snap {}".format(f))
+    local_target = "{}.{}".format(args.source, IMAGE_FILE_EXT)
+    if camera.snap('/'.join((STORAGE_DIRECTORY, local_target))):
+        remote_target = "{}_{}.{}".format(datetime.datetime.now().strftime(DATE_FORMAT), args.source, IMAGE_FILE_EXT)
+        logger.info("on_message snap {} {}".format(local_target, remote_target))
         Publisher(args.endpoint,
                   args.rootCA,
                   args.key,
                   args.cert,
                   clientID=args.clientID
-                  ).report(t, {'last_snapshot': f})
-        util.copy_to_s3(camera.filename, args.bucket, "{}{}".format(args.source,file_extension))
+                  ).report(t, {'last_snapshot': remote_target})
+        util.copy_to_s3(camera.filename, args.bucket, remote_target)
 
 
-# def on_connect(mqttc, obj, flags, rc):
-#     logger.info("on_connect {}".format(rc))
-#
-#
-# def on_subscribe(mqttc, obj, mid, granted_qos):
-#     logger.info("on_subscribe {} {}".format(mid, granted_qos))
-#
-#
-# def on_publish(mqttc, obj, mid):
-#     logger.info("on_publish {}".format(mid))
-#
-#
-# def on_log(mqttc, obj, level, string):
-#     logger.info("on_log {}".format(string))
+def on_connect(mqttc, obj, flags, rc):
+    logger.info("on_connect {}".format(rc))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    logger.info("on_subscribe {} {}".format(mid, granted_qos))
+
+
+def on_publish(mqttc, obj, mid):
+    logger.info("on_publish {}".format(mid))
+
+
+def on_log(mqttc, obj, level, string):
+    logger.info("on_log {}".format(string))
 
 
 if __name__ == "__main__":
@@ -79,10 +79,10 @@ if __name__ == "__main__":
 
     # setup callbacks
     client.on_message = on_message
-    # client.on_connect = on_connect
-    # client.on_publish = on_publish
-    # client.on_subscribe = on_subscribe
-    # client.on_log = on_log
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    client.on_subscribe = on_subscribe
+    client.on_log = on_log
 
     client.tls_set(args.rootCA,
                    certfile=args.cert,
