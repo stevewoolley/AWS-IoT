aca31067ee82d463bbce44753bc0cb1cd9f2eef9
Steve Woolley <steve.woolley@me.com>
2017-02-17 13:34:53 -0500

    redo pir

--- pir_pub.py
+++ pir_pub.py
@@ -3,16 +3,16 @@
 import argparse
 import time
 import sys
-import platform
 import logging
+import platform
 from cloud_tools import Publisher
-from pir import PIR
+from gpiozero import MotionSensor
 
 LOG_FILE = '/var/log/iot.log'
 
 
 def publicize(article):
-    logging.info("publicize {} {}".format(args.name, article))
+    logging.info("publicize pir {} {}".format(args.name, article))
     # publish to any topics
     if args.topic is not None:
         for t in args.topic:
@@ -45,38 +45,27 @@ if __name__ == "__main__":
     parser.add_argument("-k", "--key", help="Private key file path")
     parser.add_argument("-i", "--clientID", help="Client ID", default='')
 
-    parser.add_argument("-s", "--source", help="Source", default="pir")
+    parser.add_argument("-s", "--source", help="Source", required=True)
     parser.add_argument("-n", "--name", help="Thing name", default=platform.node().split('.')[0])
     parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
 
-    parser.add_argument("-z", "--active_sleep", help="sleep seconds during movement", type=float, default=2.0)
-    parser.add_argument("-y", "--passive_sleep", help="sleep seconds while quiet", type=float, default=0.5)
     parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int, required=True)
-
-    parser.add_argument("-l", "--log_level", help="Log Level", default=logging.WARNING)
-
+    parser.add_argument("-l", "--log_level", help="Log Level", default=logging.INFO)
     args = parser.parse_args()
 
     logging.basicConfig(filename=LOG_FILE, level=args.log_level)
 
-    pir = PIR(args.pin)
-    pir.start()
+    pir = MotionSensor(args.pin)
 
-    current_state = 0
     try:
+        motion = pir.motion_detected
         while True:
-            sleep = 0
-            if pir.movement():
-                if current_state == 0:
-                    current_state = 1
-                    publicize({args.source: current_state})
-                sleep = args.active_sleep
-            else:
-                if current_state == 1:
-                    current_state = 0
-                    publicize({args.source: current_state})
-                sleep = args.passive_sleep
-            # let's rest
-            time.sleep(sleep)
+            if motion is False and pir.motion_detected:
+                motion = True
+                publicize({args.source: motion})
+            elif motion is True and not pir.motion_detected:
+                motion = False
+                publicize({args.source: motion})
+                time.sleep(args.active_sleep)
     except (KeyboardInterrupt, SystemExit):
         sys.exit()
