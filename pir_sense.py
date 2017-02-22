e4814c07e745e03f361d3b9ef0d34fd5906ff8e9
Steve Woolley <steve.woolley@me.com>
2017-02-22 09:02:44 -0500

    put some options into pir tester

--- pir_sense.py
+++ pir_sense.py
@@ -11,9 +11,23 @@ if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int, required=True)
+    parser.add_argument("-q", "--queue_len",
+                        help="The length of the queue used to store values read from the sensor. (1 = disabled)",
+                        type=int, default=1)
+    parser.add_argument("-s", "--sample_rate",
+                        help="The number of values to read from the device (and append to the internal queue) per second",
+                        type=float, default=100)
+    parser.add_argument("-t", "--threshold",
+                        help="When the mean of all values in the internal queue rises above this value, the sensor will be considered “active” by the is_active property, and all appropriate events will be fired",
+                        type=float, default=0.5)
     args = parser.parse_args()
 
-    pir = MotionSensor(args.pin)
+    pir = MotionSensor(
+        args.pin,
+        queue_len=args.queue_len,
+        sample_rate=args.sample_rate,
+        threshold=args.threshold
+    )
 
     try:
         motion = False
