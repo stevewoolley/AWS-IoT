000f2be0b12a92d2ed1a1d25e3691ca471fcdfe8
Steve Woolley <steve.woolley@me.com>
2017-08-15 08:59:31 -0400

    make publisher self aware

--- publisher.py
+++ publisher.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
 import argparse
+import logging
 from cloud_tools import Publisher
+
+LOG_FILE = '/var/log/iot.log'
+
+
+def publicize(article):
+    logging.info("publisher {} {}".format(args.name, article))
+    # publish to thing
+    Publisher(
+        args.endpoint,
+        args.rootCA,
+        args.key,
+        args.cert,
+        clientID=args.clientID,
+        log_level=args.log_level
+    ).report(Publisher.THING_SHADOW.format(args.name), {Publisher.STATE: {Publisher.REPORTED: article}})
+
 
 if __name__ == "__main__":
 
@@ -13,12 +30,11 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--cert", help="Certificate file path")
     parser.add_argument("-k", "--key", help="Private key file path")
 
-    parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
     parser.add_argument("-s", "--source", help="source", default='message')
     parser.add_argument("-v", "--value", help="value", default='Foo')
     args = parser.parse_args()
 
-    for t in args.topic:
-        Publisher(args.endpoint, args.rootCA, args.key, args.cert, clientID=args.clientID).report(t,
-                                                                                                  {
-                                                                                                      args.source: args.value})
+    logging.basicConfig(filename=LOG_FILE, level=args.log_level)
+
+    publicize({args.source: args.value})
+
