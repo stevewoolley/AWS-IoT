b08185dd3251312e6f926b7f375fb639a8859136
Steve Woolley <steve.woolley@me.com>
2017-08-15 08:33:05 -0400

    make publisher self aware

--- publisher.py
+++ publisher.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python
+
 import argparse
 from cloud_tools import Publisher
 
-# parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--endpoint", help="AWS IoT endpoint", required=True)
-parser.add_argument("-i", "--clientID", help="Client ID", default=None)
-parser.add_argument("-r", "--rootCA", help="Root CA file path", required=True)
-parser.add_argument("-c", "--cert", help="Certificate file path")
-parser.add_argument("-k", "--key", help="Private key file path")
+if __name__ == "__main__":
 
-parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
-parser.add_argument("-s", "--source", help="source", default='message')
-parser.add_argument("-v", "--value", help="value", default='Foo')
-args = parser.parse_args()
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", help="AWS IoT endpoint", required=True)
+    parser.add_argument("-i", "--clientID", help="Client ID", default=None)
+    parser.add_argument("-r", "--rootCA", help="Root CA file path", required=True)
+    parser.add_argument("-c", "--cert", help="Certificate file path")
+    parser.add_argument("-k", "--key", help="Private key file path")
 
-for t in args.topic:
-    Publisher(args.endpoint, args.rootCA, args.key, args.cert, clientID=args.clientID).report(t,
-                                                                                              {args.source: args.value})
+    parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
+    parser.add_argument("-s", "--source", help="source", default='message')
+    parser.add_argument("-v", "--value", help="value", default='Foo')
+    args = parser.parse_args()
+
+    for t in args.topic:
+        Publisher(args.endpoint, args.rootCA, args.key, args.cert, clientID=args.clientID).report(t,
+                                                                                                  {
+                                                                                                      args.source: args.value})
