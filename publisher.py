6440936a152d679d1d37c3aa08c7984681777d84
Steve Woolley <steve.woolley@me.com>
2017-08-15 09:02:44 -0400

    make publisher self aware

--- publisher.py
+++ publisher.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import platform
 from cloud_tools import Publisher
 
 LOG_FILE = '/var/log/iot.log'
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     parser.add_argument("-k", "--key", help="Private key file path")
 
     parser.add_argument("-s", "--source", help="source", default='message')
+    parser.add_argument("-n", "--name", help="Thing name", default=platform.node().split('.')[0])
     parser.add_argument("-v", "--value", help="value", default='Foo')
 
     parser.add_argument("-l", "--log_level", help="Log Level", default=logging.INFO)
