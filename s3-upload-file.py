557eeb59de6a352612782747381dd4a42efccd66
Steve Woolley <steve.woolley@me.com>
2017-09-05 09:05:37 -0400

    add pattern matching to s3 file uploader

--- s3-upload-file.py
+++ s3-upload-file.py
@@ -14,7 +14,13 @@ if __name__ == "__main__":
     parser.add_argument("path", help="path to file")
     parser.add_argument("target", help="target file name", default=[], nargs='*')
     parser.add_argument('--delete', help="delete source file when complete", action='store_true')
+    parser.add_argument("-m", "--match", help="perform only if pattern match")
     args = parser.parse_args()
+
+    # if pattern match given, only proceed if match
+    if args.match:
+        if args.match not in args.path:
+            sys.exit(0)
 
     # use same filename if target not given
     if args.target:
