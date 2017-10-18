934a720656ebb0382c11e0998d682dfce05c5f5b
Steve Woolley <steve.woolley@me.com>
2017-10-18 13:55:10 -0400

    debug

--- sensor_pub.py
+++ sensor_pub.py
@@ -17,8 +17,7 @@ LOG_FILE = '/var/log/iot.log'
 def high():
     if args.cushion > 0.0:
         time.sleep(args.cushion)
-        logging.info("sensor_pub high active time {} {}".format(sensor.active_time, args.cushion))
-        if sensor.active_time >= args.cushion:
+        if sensor.is_pressed:
             publicize({SOURCE: args.source,
                        MESSAGE: args.high_value,
                        ALERT_COUNT: args.high_alert})
@@ -31,8 +30,7 @@ def high():
 def low():
     if args.cushion > 0.0:
         time.sleep(args.cushion)
-        logging.info("sensor_pub low active time {} {}".format(sensor.active_time, args.cushion))
-        if sensor.active_time is None:
+        if not sensor.is_pressed:
             publicize({SOURCE: args.source,
                        MESSAGE: args.low_value,
                        ALERT_COUNT: args.low_alert})
